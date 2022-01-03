6b799196a4645368548aa18f1c9ea1738d3e23ce
Rishav Thakur <rishav.21m@gmail.com>
2022-01-03 23:03:41 +0530

    intial commit

--- subscriberform.php
+++ subscriberform.php
@@ -4,7 +4,7 @@
 if($_POST["message"]) {
 
 
-mail("chitraksh2705@gmail.com", "Here is the subject line",
+mail("apniudaan.orai@gmail.com", "Here is the subject line",
 
 
 $_POST["yoo"]. "From: an@email.address");
